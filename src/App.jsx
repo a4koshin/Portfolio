30bcf6c03e2e0f99308ba98029733dc640660c16
a4kooshinn <mankajr11@gmail.com>
2025-08-25 18:04:06 +0300

    Fix ContactPage filename casing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
-import ContactPage from "./pages/ContactPage";
+import Contact from "./pages/Contact";
 const App = () => {
   return (
     <div className="min-h-screen  ">
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <Footer />
--- src/pages/contactPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContactPage = () => {
-  return <div className="mx-auto max-w-7xl mt-16 px-4">contactPage</div>;
-};
-
-export default ContactPage;
