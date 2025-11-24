4672fd378c6d92e3083ace595e450bf06415179a
a4koshin <mankajr11@gmail.com>
2025-11-24 14:29:59 +0300

    Changed the Header background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,45 +12,47 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="relative z-50 mx-auto max-w-7xl px-4 flex items-center justify-between">
-      <Link
-        to={"/"}
-        onClick={() => setIsOpen(false)}
-        className="flex items-center"
-      >
-        <img
-          src={logo}
-          alt="Logo"
-          className="w-24 h-24 md:h-32 md:w-32 rounded-full"
-        />
-      </Link>
+    <nav className="fixed top-0 left-0 w-full bg-white/30 backdrop-blur-md z-50 shadow">
+      <div className="max-w-7xl mx-auto px-4 flex items-center justify-between">
+        <Link
+          to={"/"}
+          onClick={() => setIsOpen(false)}
+          className="flex items-center"
+        >
+          <img
+            src={logo}
+            alt="Logo"
+            className="w-24 h-24 md:h-32 md:w-32 rounded-full"
+          />
+        </Link>
 
-      <button className=" relative z-50" onClick={toggleMenuHandler}>
-        {isOpen ? (
-          <HiOutlineMinus className="size-8 text-gray-800" />
-        ) : (
-          <HiOutlineMenuAlt4 className="size-8 text-gray-800" />
+        <button className="relative z-50" onClick={toggleMenuHandler}>
+          {isOpen ? (
+            <HiOutlineMinus className="size-8 text-gray-800" />
+          ) : (
+            <HiOutlineMenuAlt4 className="size-8 text-gray-800" />
+          )}
+        </button>
+
+        {isOpen && (
+          <div className="absolute top-20 right-0 w-60 h-80 bg-white/30 backdrop-blur-lg p-4 flex flex-col items-center space-y-4 shadow-md z-40 rounded-b-2xl">
+            {navigation.map((item) => (
+              <NavLink
+                key={item.id}
+                to={item.href}
+                onClick={() => setIsOpen(false)}
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-md font-medium bg-blue-500 px-6 py-2 rounded-md text-white"
+                    : "text-md font-medium text-gray-600 hover:text-blue-500"
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
+          </div>
         )}
-      </button>
-
-      {isOpen && (
-        <div className="absolute top-20 right-0 w-60 h-80 bg-white/30 backdrop-blur-lg p-4 flex flex-col items-center space-y-4  shadow-md z-40 rounded-b-2xl">
-          {navigation.map((item) => (
-            <NavLink
-              key={item.id}
-              to={item.href}
-              onClick={() => setIsOpen(false)}
-              className={({ isActive }) =>
-                isActive
-                  ? "text-md font-medium bg-blue-500 px-6 py-2 rounded-md text-white"
-                  : "text-md font-medium text-gray-600 hover:text-blue-500"
-              }
-            >
-              {item.name}
-            </NavLink>
-          ))}
-        </div>
-      )}
+      </div>
     </nav>
   );
 };
