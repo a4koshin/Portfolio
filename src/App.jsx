48c844cb1122f2f338651d092ddc442f0e5a66b7
a4koshin <mankajr11@gmail.com>
2025-11-25 21:38:01 -0400

    Removing the background color

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contact from "./pages/Contact";
 import ScrollToTop from "./components/ScrollToTop";
 const App = () => {
   return (
-    <div className="min-h-screen mx-auto max-w-7xl px-4 pt-24 md:pt-12 bg-red-500 ">
+    <div className="min-h-screen mx-auto max-w-7xl px-4 pt-24 md:pt-12 ">
       <ScrollToTop />
       <Navbar />
 
